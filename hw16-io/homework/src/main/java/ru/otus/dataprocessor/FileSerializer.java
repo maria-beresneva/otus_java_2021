060d4eb8f00c90ffa3c1682495b559664858e0a8
maria.beresneva <maria.beresneva@gett.com>
2021-12-22 08:01:53 +0300

    Homework for json serialisation

--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,22 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.*;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final String fileNameForOutput;
 
     public FileSerializer(String fileName) {
+        fileNameForOutput = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        //формирует результирующий json и сохраняет его в файл
+        try(FileOutputStream fileOutputStream = new FileOutputStream(fileNameForOutput)){
+            new ObjectMapper().writeValue(fileOutputStream, data);
+        } catch (IOException e) {
+            throw new UnsupportedOperationException(e);
+        }
     }
 }
--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,34 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.otus.model.Measurement;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
+    private final String fileName;
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        try {
+            Iterator<JsonNode> jsonTree = new ObjectMapper()
+                    .readTree(getClass().getClassLoader().getResourceAsStream(fileName)).elements();
+            List<Measurement> measurements = new ArrayList<>();
+            while(jsonTree.hasNext()) {
+                JsonNode jsonNode = jsonTree.next();
+                measurements.add(new Measurement(jsonNode.get("name").asText(), jsonNode.get("value").asDouble()));
+            }
+            return measurements;
+        } catch (IOException e) {
+            throw new ClassCastException();
+        }
     }
 }
