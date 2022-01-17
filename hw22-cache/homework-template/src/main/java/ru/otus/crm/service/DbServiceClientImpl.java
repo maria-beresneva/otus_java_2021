1524a178de5258a50abddc2cfb59e65e2287b979
maria.beresneva <maria.beresneva@gett.com>
2022-01-17 11:07:13 +0300

    Add homework

--- hw22-cache/homework-template/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw22-cache/homework-template/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -1,20 +1,22 @@
 package ru.otus.crm.service;
 
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.HwListener;
+import ru.otus.cachehw.MyCache;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.crm.model.Client;
 import ru.otus.core.sessionmanager.TransactionManager;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.Optional;
 
 public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+    @Getter
+    private final HwCache<String, Client> clientCache = new MyCache<>();
 
     private final DataTemplate<Client> clientDataTemplate;
     private final TransactionManager transactionManager;
@@ -23,6 +25,13 @@ public class DbServiceClientImpl implements DBServiceClient {
         this.transactionManager = transactionManager;
         this.clientDataTemplate = clientDataTemplate;
 
+        HwListener<String, Client> listener = new HwListener<String, Client>() {
+            @Override
+            public void notify(String key, Client value, String action) {
+                log.info("key:{}\nvalue:{}\naction: {}", key, value, action);
+            }
+        };
+        clientCache.addListener(listener);
     }
 
     @Override
@@ -31,10 +40,18 @@ public class DbServiceClientImpl implements DBServiceClient {
             var clientCloned = client.clone();
             if (client.getId() == null) {
                 clientDataTemplate.insert(session, clientCloned);
+
+                log.info("Caching created user: {}", clientCloned.getId());
+                clientCache.put(String.valueOf(clientCloned.getId()), clientCloned);
+
                 log.info("created client: {}", clientCloned);
                 return clientCloned;
             }
             clientDataTemplate.update(session, clientCloned);
+
+            log.info("Update user in cash: {}", clientCloned.getId());
+            clientCache.put(String.valueOf(clientCloned.getId()), clientCloned);
+
             log.info("updated client: {}", clientCloned);
             return clientCloned;
         });
@@ -42,8 +59,13 @@ public class DbServiceClientImpl implements DBServiceClient {
 
     @Override
     public Optional<Client> getClient(long id) {
+        Client client = clientCache.get(String.valueOf(id));
+        if (client != null)
+            return Optional.of(client);
+
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientOptional = clientDataTemplate.findById(session, id);
+            clientOptional.ifPresent(value -> clientCache.put(String.valueOf(id), value));
             log.info("client: {}", clientOptional);
             return clientOptional;
         });
@@ -53,6 +75,7 @@ public class DbServiceClientImpl implements DBServiceClient {
     public List<Client> findAll() {
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientList = clientDataTemplate.findAll(session);
+            clientList.forEach(client -> clientCache.put(String.valueOf(client.getId()), client));
             log.info("clientList:{}", clientList);
             return clientList;
        });
